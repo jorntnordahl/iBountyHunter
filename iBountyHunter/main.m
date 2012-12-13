02455177478098062baaa907e98604e52283bcc6
Jorn Nordahl <JornTNordahl@gmail.com>
2012-12-13 07:40:27 -0800

    Initial Commit

---
+++ iBountyHunter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iBountyHunter
+//
+//  Created by Jorn Nordahl on 12/13/12.
+//  Copyright (c) 2012 Jorn Nordahl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
